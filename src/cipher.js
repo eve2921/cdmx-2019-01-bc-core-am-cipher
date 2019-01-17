4f2aa8d37e34c32fb6a2d7bf670658a5b717ce49
eve2921 <eve2921@hotmail.com>
2019-01-17 08:10:30 -0600

    por fi guardate

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,29 @@
 window.cipher = {
-  encode:() => {
-    }
-    
-};
-add
+/*
+  encode(){
+    },
+  
+  decode(){
+    } 
+*/
+
+/*encode(){
+    var miLetraEnAscii = String.fromCharCode(65);
+    document.getElementById("encriptando").innerHTML = miLetraEnAscii;
+    console.log(A);
+
+}*/
+encode(){
+let miLetraEnAscii = 'a'
+let ascii_value = miLetraEnAscii.charCodeAt(0)
+console.log(ascii_value);
+}
+
+
+
+
+
+  };
+
+
+
